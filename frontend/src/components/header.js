476c483f70a9e0eac23ad30a3f7d2d5a77f6c70f
Roni <rtlockwoodwork@gmail.com>
2023-03-20 19:20:49 -0600

    fix logo size on moble for navbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -6,7 +6,7 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle, siteDescription }) => (
 <header className="m-auto pt-8 mb-8 ">
-          <div className="grid align-center text-black  text-center ">
+          <div className="w-3/4 md:w-full grid align-center text-black  text-center ">
             <Link to="/"><StaticImage width={550} src="../images/Logo-THA.png" alt="logo" className="max-w-[550px] w-1/2 lg:w-1/3 m-auto"/></Link>
           </div>
         <Hamburger />     
