356f1ea183f22e3defc1c5d2db50d106474d8ea0
Roni <rtlockwoodwork@gmail.com>
2023-08-28 18:44:56 -0600

    fix width of hero img and clean up hamburger

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -6,8 +6,8 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle, siteDescription }) => (
 <header className="m-auto pt-8 mb-8 ">
-          <div className="w-3/4 md:w-full grid align-center text-black  text-center ">
-            <Link to="/"><StaticImage width={550} src="../images/Logo-THA.png" alt="logo" className="max-w-[550px] w-1/2 lg:w-1/3 m-auto"/></Link>
+          <div className="w-5/6 mx-auto grid align-center text-black  text-center ">
+            <Link to="/" ><StaticImage width={550} src="../images/Logo-THA.png" alt="logo" className="max-w-[550px] w-1/2 lg:w-1/3 m-auto"/></Link>
           </div>
         <Hamburger />     
 </header>
